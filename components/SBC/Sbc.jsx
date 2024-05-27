31aa59f1f8db37915b6e8b8e7b25ede27f32f272
NumaanisCoder <nomannaeem985@gmail.com>
2024-05-27 17:45:05 +0530

    update

--- components/SBC/Sbc.jsx
+++ components/SBC/Sbc.jsx
@@ -7,7 +7,7 @@ const Sbc = ({data}) => {
     const isDarkMode = useSelector(state=> state.theme.isDarkMode);
     const {title,image} = data;
     const encrypturl = title.replace(/-/,'~');
-    const urlpart =  `/blog/${encrypturl.replace(/ /g,'-')}`;
+    const urlpart =  `/article/${encrypturl.replace(/ /g,'-')}`;
   
 
   return (
