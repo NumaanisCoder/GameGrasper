4f625dfc84c864fca6381b3fe976d9bfdc445992
NumaanisCoder <nomannaeem985@gmail.com>
2024-09-16 12:44:53 +0530

    newOptimizeUpdate

--- pages/article/[title].js
+++ pages/article/[title].js
@@ -1,32 +1,27 @@
 import React, { useEffect, useState } from "react";
-import NavBar from "@/components/NavBar/NavBar";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import style from "@/styles/BlogPage.module.css";
 import Head from "next/head";
-import Sbc from "@/components/SBC/Sbc";
 import getEmoji from "@/lib/Emoji";
 import Image from "next/image";
 import Link from "next/link";
 import { useSelector } from "react-redux";
-import ThemeButton from "@/components/ToggelTheme/ThemeButton";
-import AdBanner from "@/components/AdBanner";
-import SingleComment from "@/components/comment/SingleComment";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { useSnackbar } from "notistack";
-import Tag from "@/components/tag/Tag";
-import SocialShare from "@/components/SocialShare";
 import { FaShare } from "react-icons/fa";
-import FollowUs from "@/components/FollowUs";
 
-
-
-
-
+// Dynamically import components
+const Sbc = dynamic(() => import("@/components/SBC/Sbc"));
+const AdBanner = dynamic(() => import("@/components/AdBanner"));
+const SingleComment = dynamic(() => import("@/components/comment/SingleComment"));
+const Tag = dynamic(() => import("@/components/tag/Tag"));
+const SocialShare = dynamic(() => import("@/components/SocialShare"));
+const FollowUs = dynamic(() => import("@/components/FollowUs"));
 
 const Blog = (props) => {
   const { enqueueSnackbar } = useSnackbar();
-  
   const token = Cookies.get('token');
   const router = useRouter();
 
@@ -42,7 +37,7 @@ const Blog = (props) => {
     comment,
   } = props.data.message;
 
-  const [AllComments, setAllComments] = useState(comment)
+  const [AllComments, setAllComments] = useState(comment);
   const [Comment, setComment] = useState({
     message: "",
     token: "",
@@ -54,9 +49,9 @@ const Blog = (props) => {
   const [isMobile, setIsMobile] = useState(false);
   const [scroll, setScroll] = useState(0);
 
-    const encrypturl = title.replace(/-/g, '~');
-    const questionmark = encrypturl.replace(/\?/g, '$');
-    const urlpart = `/article/${questionmark.replace(/ /g, '-')}`
+  const encrypturl = title.replace(/-/g, '~');
+  const questionmark = encrypturl.replace(/\?/g, '$');
+  const urlpart = `/article/${questionmark.replace(/ /g, '-')}`;
 
   useEffect(() => {
     const handleResize = () => {
@@ -66,33 +61,25 @@ const Blog = (props) => {
     handleResize();
     window.addEventListener("resize", handleResize);
 
-   
-
     let progressBarHandler = () => {
-            
       const totalScroll = document.documentElement.scrollTop;
       const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
       const scroll = `${totalScroll / windowHeight}`;
 
       setScroll(scroll);
-  }
+    };
 
-  window.addEventListener("scroll", progressBarHandler);
+    window.addEventListener("scroll", progressBarHandler);
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  
 
   function formatDate(milliseconds) {
     const date = new Date(milliseconds);
     return date.toUTCString();
   }
 
-
   const arrayoftags = tags && tags.split(',').map(tag => tag.trim());
-
-
 
   return (
     <article className={`${style.root} ${isDarkMode ? style.dark : ""}`}>
@@ -115,12 +102,10 @@ const Blog = (props) => {
       </Head>
 
       <div className={style.progressBarContainer}>
-        <div className={style.progressBar} style={{transform: `scale(${scroll}, 1)`}}></div>
+        <div className={style.progressBar} style={{ transform: `scale(${scroll}, 1)` }}></div>
       </div>
-  
 
       <main className={`${style.parent} ${isDarkMode ? style.darkp : ""}`}>
-      
         <div className={style.metaInfo}>
           <h1 className={style.title}>{title}</h1>
           <p className={style.metap}>
@@ -131,42 +116,39 @@ const Blog = (props) => {
           </p>
         </div>
 
-            {/* Adsterra Ad */}
-
         <Image
-  src={image}
-  className={style.blogimage}
-  alt={title}
-  layout="responsive"
-  width={700} // Width for aspect ratio (can be any values, Next.js will handle scaling)
-  height={500} // Height for aspect ratio
-  quality={65} // Adjust quality for faster loading
-  loading="lazy"
-/>
+          src={image}
+          className={style.blogimage}
+          alt={title}
+          layout="responsive"
+          width={700}
+          height={500}
+          quality={65}
+          loading="lazy"
+        />
+
         <div className={style.socialMediaContainer}>
-     <FaShare width={30}/> <SocialShare url={`https://www.gamegrasper.blog${urlpart}`} title={title}/>
+          <FaShare width={30} />
+          <SocialShare url={`https://www.gamegrasper.blog${urlpart}`} title={title} />
         </div>
 
-      <p
-        className={`${style.content} ${isDarkMode ? style.darkContent : ""}`}
-        dangerouslySetInnerHTML={{ __html: content }}
-      ></p>
+        <p
+          className={`${style.content} ${isDarkMode ? style.darkContent : ""}`}
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></p>
+
         <AdBanner data-ad-slot="5158369704" data-ad-format="auto" data-full-width-responsive="true" />
 
-        
         <div className={style.tagContainer}>
-          {arrayoftags && <span>Tags:</span>} {arrayoftags && arrayoftags.map((value, index) => (
-            <Tag text={value} key={index}/>
-          ))}
+          {arrayoftags && <span>Tags:</span>}{" "}
+          {arrayoftags && arrayoftags.map((value, index) => <Tag text={value} key={index} />)}
         </div>
-
 
         <div className={style.socialMediaContainer}>
-        Follow us on <FollowUs/>
+          Follow us on <FollowUs />
         </div>
-        
 
-        <h3 className={style.commentSecTitle}>Comment Section </h3>
+        <h3 className={style.commentSecTitle}>Comment Section</h3>
         <section className={style.commentSection}>
           <div className={style.viewComments}>
             {AllComments && AllComments.map((data, index) => (
@@ -181,7 +163,6 @@ const Blog = (props) => {
                 e.preventDefault();
               }}
             >
-          
               <textarea
                 name="comment"
                 value={Comment.message}
@@ -201,7 +182,7 @@ const Blog = (props) => {
                     router.push("/accounts/signup");
                   }
 
-                  if (Comment.message < 1) {
+                  if (Comment.message.length < 1) {
                     return;
                   }
                   e.target.innerText = "Posting..";
@@ -223,16 +204,12 @@ const Blog = (props) => {
             </form>
           </div>
         </section>
-      
       </main>
-
 
       <AdBanner data-ad-slot="5158369704" data-ad-format="auto" data-full-width-responsive="true" />
       {props.data.rem.length > 0 && (
         <section className={`${style.suggestionparent} ${isDarkMode ? style.darksp : ""}`}>
-          <p className={`${style.sphead} ${isDarkMode ? style.darkSpHead : ""}`}>
-            Latest
-          </p>
+          <p className={`${style.sphead} ${isDarkMode ? style.darkSpHead : ""}`}>Latest</p>
           <div className={style.rContainer}>
             {props.data.rem.map((value, index) => (
               <Sbc data={value} key={index} />
@@ -247,11 +224,13 @@ const Blog = (props) => {
 export async function getServerSideProps(context) {
   const { query } = context;
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blog/${query.title}`, {
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blog/${query.title}`,
+    {
       headers: {
         'Cache-Control': 'public, max-age=86400, immutable',
       }
-});
+    }
+  );
   const data = await res.json();
 
   return {
